b755e452792bd1621714bf363dd2b57b5aa72d61
gianmarco99lc <gianmarcoleonecamelio@gmail.com>
2024-01-16 20:02:05 -0400

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    cors: true,
+    proxy: {
+      '/api': {
+        target: "https://crucial-healthy-dinosaur.ngrok-free.app/cmcapp-backend-1.0/api/v1/",
+        changeOrigin: true,
+        secure: true,
+        ws: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
